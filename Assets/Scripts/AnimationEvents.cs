a4c5a7d3b8504dd9fd155f260e8dfd4f98f80647
Rubalom <Rubalom14228@gmail.com>
2020-08-19 17:28:47 +0300

    Initial commit

---
+++ Assets/Scripts/AnimationEvents.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEvents : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    public void OnAttackEnd()
+    {
+        animator.SetBool("IsAttacking", false);
+        Debug.Log("Attack ended");
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 40f;
+
+    [SerializeField] private Animator animator;
+    [SerializeField] private CharacterController2D controller;
+    private Rigidbody2D rb;
+
+    private float horizontalMove = 0f;
+    private bool jump = false;
+    private bool crouch = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal") * speed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            animator.SetBool("IsAttacking", true);
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+}
