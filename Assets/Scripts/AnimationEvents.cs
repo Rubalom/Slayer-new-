a74511a105b4251f96a25f0e99cf19fcbf711fcd
Rubalom <Rubalom14228@gmail.com>
2020-08-26 20:29:24 +0300

    URP test

--- Assets/Scripts/AnimationEvents.cs
+++ Assets/Scripts/AnimationEvents.cs
@@ -1,14 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class AnimationEvents : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private TMP_Text text;
 
     public void OnAttackEnd()
     {
         animator.SetBool("IsAttacking", false);
         Debug.Log("Attack ended");
+    }
+
+    public void OnAttackingBegin()
+    {
+        text.text = "ATTACKING";
+    }
+
+    public void OnIdleBegin()
+    {
+        text.text = "IDLE";
+    }
+
+    public void OnRunningBegin()
+    {
+        text.text = "RUNNING";
     }
 }
